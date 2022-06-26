5db61a993a8a6fbe69d2d2f4972db0a4e94c12b7
Jacob Haflett <hafletjm01@gmail.com>
2022-06-26 14:20:32 -0500

    update homepage

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5fccc179ffa4a7c690e4e9269724a8d.js"
+  "/precache-manifest.72138686165163e3e27cab193cf4b47c.js"
 );
 
 self.addEventListener('message', (event) => {
