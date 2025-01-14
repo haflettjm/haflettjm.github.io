0d85bb7e4fa83586f0b78a2e1146617b95c779fe
haflettjm <hafletjm01@gmail.com>
2025-01-14 02:34:42 -0800

    initial commit after reset

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  modules: ['@nuxtjs/tailwindcss'],
+})
